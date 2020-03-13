02c59006d4c928a0a4e1c0c926f44764b54c2e95
Anoop MS <1988anoopms@gmail.com>
2020-03-13 06:17:24 +0530

    Inital header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { APP_BASE_HREF } from "@angular/common";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }]
 })
-export class AppModule { }
+export class AppModule {}
